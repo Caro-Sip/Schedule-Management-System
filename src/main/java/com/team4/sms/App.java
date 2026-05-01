9eceeacd5b0cadc0c4d4382ae9c497d34e549f4c
caro <suoncaro@gmail.com>
2026-05-01 14:10:21 +0700

    Initialized maven project ; added dependencies and plugins ; made directories for code strucutre

---
+++ src/main/java/com/team4/sms/App.java
@@ -0,0 +1,14 @@
+package com.team4.sms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "This is Cambodiaaaaa\n\n\n\n" );
+    }
+}
